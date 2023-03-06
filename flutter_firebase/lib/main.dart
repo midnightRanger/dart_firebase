9591b0c95bcfd7530cb08aa62b1e5615d4efc83d
Lev Ostapenko <012938576a@gmail.com>
2023-03-06 11:57:54 +0300

    added enum for exceptions

---
+++ flutter_firebase/lib/main.dart
@@ -0,0 +1,298 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/firebase_options.dart';
+import 'package:flutter_firebase/utils/exception_codes.dart';
+import 'package:flutter_firebase/widget/dynamic_input_widget.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Authorization',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Firebase auth'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+   // Define Form key
+  final _formKey = GlobalKey<FormState>();
+
+  // Instantiate validator
+  // final AuthValidators authValidator = AuthValidators();
+
+// controllers
+  late TextEditingController emailController;
+  late TextEditingController usernameController;
+  late TextEditingController passwordController;
+  late TextEditingController confirmPasswordController;
+
+// create focus nodes
+  late FocusNode emailFocusNode;
+  late FocusNode usernameFocusNode;
+  late FocusNode passwordFocusNode;
+  late FocusNode confirmPasswordFocusNode;
+
+  // to obscure text default value is false
+  bool obscureText = true;
+  // This will require to toggle between register and sigin in mode
+  bool registerAuthMode = false;
+  bool _isAnonymous = false;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    usernameController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmPasswordController = TextEditingController();
+
+    emailFocusNode = FocusNode();
+    usernameFocusNode = FocusNode();
+    passwordFocusNode = FocusNode();
+    confirmPasswordFocusNode = FocusNode();
+
+    super.initState();
+  }
+
+   @override
+  void dispose() {
+    super.dispose();
+
+    emailController.dispose();
+    usernameController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+
+    emailFocusNode.dispose();
+    usernameFocusNode.dispose();
+    passwordFocusNode.dispose();
+    confirmPasswordFocusNode.dispose();
+  }
+
+  void login() async {
+    String message; 
+    try {
+
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+    
+    message = userCredential != null ? "Email: ${userCredential.user!.email}, ${userCredential.user!.displayName}" : "Sorry, something went wrong";
+    } on FirebaseException catch (e) {
+       message = getExceptionCode(e.code).exceptionMessage;
+    }
+
+       AlertDialog alert = AlertDialog(
+          title: const Text('Information: '),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                  Navigator.of(context, rootNavigator: true).pop();
+                
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return alert;
+          },
+        );
+  }
+
+// Create a function that'll toggle the password's visibility on the relevant icon tap.
+  void toggleObscureText() {
+    setState(() {
+      obscureText = !obscureText;
+    });
+  }
+
+
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child:
+
+       Padding(
+      padding: const EdgeInsets.all(10),
+      child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+
+              // Email
+              DynamicInputWidget(
+                controller: emailController,
+                obscureText: false,
+                focusNode: emailFocusNode,
+                toggleObscureText: null,
+                prefIcon: const Icon(Icons.mail),
+                labelText: "Enter Email Address",
+                textInputAction: TextInputAction.next,
+                isNonPasswordField: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Username
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                height: registerAuthMode ? 65 : 0,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 500),
+                  opacity: registerAuthMode ? 1 : 0,
+                  child: DynamicInputWidget(
+                    controller: usernameController,
+                    obscureText: false,
+                    focusNode: usernameFocusNode,
+                    toggleObscureText: null,
+                    prefIcon: const Icon(Icons.person),
+                    labelText: "Enter Username(Optional)",
+                    textInputAction: TextInputAction.next,
+                    isNonPasswordField: true,
+                  ),
+                ),
+              ),
+
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: registerAuthMode ? 1 : 0,
+                child: const SizedBox(
+                  height: 20,
+                ),
+              ),
+
+              DynamicInputWidget(
+                controller: passwordController,
+                labelText: "Enter Password",
+                obscureText: obscureText,
+                focusNode: passwordFocusNode,
+                toggleObscureText: toggleObscureText,
+                prefIcon: const Icon(Icons.password),
+                textInputAction: registerAuthMode
+                    ? TextInputAction.next
+                    : TextInputAction.done,
+                isNonPasswordField: false,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                height: registerAuthMode ? 65 : 0,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 500),
+                  opacity: registerAuthMode ? 1 : 0,
+                  child: DynamicInputWidget(
+                    controller: confirmPasswordController,
+                    focusNode: confirmPasswordFocusNode,
+                    isNonPasswordField: false,
+                    labelText: "Confirm Password",
+                    obscureText: obscureText,
+                    prefIcon: const Icon(Icons.password),
+                    textInputAction: TextInputAction.done,
+                    toggleObscureText: toggleObscureText,
+                    
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Cancel'),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (!registerAuthMode) {
+                        login();
+                      }
+                       
+                        
+                    },
+                    child: Text(registerAuthMode ? 'Register' : 'Sign In'),
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all(8.0),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(registerAuthMode
+                      ? "Already Have an account?"
+                      : "Don't have an account yet?"),
+                  TextButton(
+                    onPressed: () {
+                      setState(() => registerAuthMode = !registerAuthMode);
+                    },
+                    child: Text(registerAuthMode ? "Sign In" : "Regsiter"),
+                  ),
+                
+                SizedBox(width: 20),
+                IconButton(onPressed: () {
+                  setState(() {
+                    !_isAnonymous;
+
+                  });
+                }, icon: _isAnonymous ? Icons.location_history_rounded)
+            ],
+          )]),
+ 
+
+      ))));
+  }
+}
---
+++ flutter_firebase/lib/utils/exception_codes.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum ExceptionCodes {
+  weak_password,
+  email_already_in_user,
+  user_not_found,
+  wrong_password,
+  something_went_wrong
+}
+
+extension ExceptionCodesExtension on ExceptionCodes {
+  String get exceptionMessage {
+    switch (this) {
+      case ExceptionCodes.email_already_in_user:
+        return "Такой E-mail уже используется!";
+      case ExceptionCodes.user_not_found:
+        return "Пользователь с такими данными не найден";
+      case ExceptionCodes.weak_password:
+        return "Пароль не удовлетворяет валидации";
+      case ExceptionCodes.wrong_password:
+        return "Указанный пароль не подходит";
+
+      default:
+        return "Что-то пошло не так..";
+    }
+  }
+}
+
+ExceptionCodes getExceptionCode(String code) {
+  for (final e in ExceptionCodes.values) {
+    if (e.name == code) return e;
+  }
+  return ExceptionCodes.something_went_wrong;
+}
